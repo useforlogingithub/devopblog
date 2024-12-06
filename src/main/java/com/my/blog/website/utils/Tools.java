9305a87f06284fb2b26b0ca6d8f90551ce992c8e
lirencong <373098492@qq.com>
2024-12-06 20:47:31 +0800

    first commit

--- src/main/java/com/my/blog/website/utils/Tools.java
+++ src/main/java/com/my/blog/website/utils/Tools.java
@@ -1,7 +1,5 @@
 package com.my.blog.website.utils;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
@@ -10,6 +8,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
+import java.util.Base64;
 import java.util.Random;
 
 /**
@@ -57,14 +56,14 @@ public class Tools {
         SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
         cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
         byte[] encryptedBytes = cipher.doFinal(data.getBytes());
-        return new BASE64Encoder().encode(encryptedBytes);
+        return Base64.getEncoder().encodeToString(encryptedBytes);
     }
 
     public static String deAes(String data, String key) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
         cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-        byte[] cipherTextBytes = new BASE64Decoder().decodeBuffer(data);
+        byte[] cipherTextBytes = Base64.getDecoder().decode(data);
         byte[] decValue = cipher.doFinal(cipherTextBytes);
         return new String(decValue);
     }
--- src/test/java/com/my/blog/website/IndexControllerTest.java
+++ src/test/java/com/my/blog/website/IndexControllerTest.java
@@ -1,4 +1,4 @@
-package com.my.blog.website.controller;
+package com.my.blog.website;
 
 import org.junit.Ignore;
 import org.junit.Test;
@@ -6,15 +6,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import static org.junit.Assert.*;
 
 /**
  * 接口测试方法
